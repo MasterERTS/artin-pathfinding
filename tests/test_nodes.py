d7f2634bf5136873317e20d8b94dd875ae7c09b3
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-29 14:49:50 +0100

    ✅ add Node object tests

---
+++ tests/test_nodes.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author : Erwin Lejeune <erwin.lejeune15@gmail.com>
+@date : 28/01/2020
+@brief : Node object tests
+'''
+
+import numpy as np
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from lib.world import World
+from lib.node import Node
+from lib.pathfinder import *
+
+
+def test_uniqueness():
+    env = World(10, 10, 0.2)
+    free_tiles = []
+    for i in range(env.L*env.H):
+        if env.is_accessible(i):
+            free_tiles.append(i)
+    nodes = [Node(i, 84, 0, None, env) for i in free_tiles]
+
+    # list of positions
+    pos = []
+    for elem in nodes:
+        pos.append(elem.tile_pos)
+
+    # check uniqueness
+    seen = set()
+    uniqueness = not any(k in seen or seen.add(k) for k in pos)
+
+    assert(uniqueness == True)
+
+def test_outofbounds():
+    env = World(10, 10, 0.2)
+    free_tiles = []
+    for i in range(env.L*env.H):
+        if env.is_accessible(i):
+            free_tiles.append(i)
+    new_node = [Node(i, 84, 0, None, env) for i in free_tiles]
+
+    for elem in new_node:
+        assert(elem.tile_pos >= 0 and elem.tile_pos <= (env.L*env.H))
+
+def test_availability():
+    env = World(10, 10, 0.2)
+    free_tiles = []
+    for i in range(env.L*env.H):
+        if env.is_accessible(i):
+            free_tiles.append(i)
+    new_node = [Node(i, 84, 0, None, env) for i in free_tiles]
+
+    for elem in new_node:
+        assert(env.w[elem.tile_pos] != 1)
