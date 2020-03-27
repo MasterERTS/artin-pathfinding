91bf14018bb108ecfde2c6a627dd632738b81f51
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-27 17:58:23 +0100

    ✅ tests successful

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ from lib.dfs import DepthFirstSearch
 from lib.astar import AStar
 from lib.node import Node
 from lib.world import World
+from lib.dataviz import PathfindingComparator
 
 # --------------------------------------------- #
 
@@ -105,60 +106,6 @@ def dfs(env, allow_diagonals, display):
     return(len(pathfinder.path))
 
 
-'''
-Bidirectional A* is (with this env configuration) 
-'''
-
-
-def comparative_test(allow_diag):
-    continuing = True
-    while continuing:
-        env = World(40, 20, 0)
-        env.display()
-        env.display_available_pos()
-
-        first = env.get_start()
-        last = env.get_target()
-
-        zero_time_bd_astar = time.clock()
-        pathfinder = TwoWayAStar(first, last, allow_diag, env)
-        pathfinder.shortest_path()
-        computation_time_bd_astar = time.clock() - zero_time_bd_astar
-
-        if(not(pathfinder.reached)):
-            continuing = False
-            continue
-
-        pathfinder.path_info()
-        env.display_path(pathfinder.path)
-
-        time.sleep(2)
-
-        zero_time_astar = time.clock()
-        pathfinder_bis = AStar(first, last, allow_diag, env)
-        pathfinder_bis.shortest_path()
-
-        computation_time_astar = time.clock() - zero_time_astar
-        pathfinder_bis.path_info()
-
-        env.display_path(pathfinder_bis.path)
-
-        time.sleep(2)
-
-        print("Computation time for BiDir A* = " +
-              str(computation_time_bd_astar) + ' seconds.')
-        print("Computation time for A* = " +
-              str(computation_time_astar) + ' seconds.')
-
-        if computation_time_astar > computation_time_bd_astar:
-            print("A* is slower than 2-Way A* by " +
-                  str(int(computation_time_astar/computation_time_bd_astar*100)) + "%")
-        else:
-            print("2-Way A* is slower than A* by " +
-                  str(int(computation_time_bd_astar/computation_time_astar*100)) + "%")
-        break
-
-
 def dataviz(origin_env, allow_diagonals, test_samples, fig_title):
     display = False
     path_astar = []
@@ -181,21 +128,19 @@ def dataviz(origin_env, allow_diagonals, test_samples, fig_title):
         path_stw.append(spanningtreewalk(env, allow_diagonals, False))
         path_dij.append(dijkstra(env, allow_diagonals, False))
         env = World(origin_env.L, origin_env.H, origin_env.pWalls)
+    
+    views = PathfindingComparator()
+    views.addFigure(3, fig_title)
+    views.addPlotToAxs(path_astar, fig_title, 0, "AStar")
+    views.addPlotToAxs(path_dij, fig_title, 1, "Dijkstra")
+    views.addPlotToAxs(path_bidir, fig_title, 2, "Bidirectional AStar")
 
-    fig, axs = plt.subplots(6, sharex = True, sharey = True)
-    fig.suptitle(fig_title)
-    axs[0].plot(path_astar, c=np.random.rand(3,))
-    axs[0].set_title("AStar Paths")
-    axs[1].plot(path_bidir, c=np.random.rand(3,))
-    axs[1].set_title("Bidirectional AStar Paths")
-    axs[2].plot(path_dij, c=np.random.rand(3,))
-    axs[2].set_title("Dijkstra Paths")
-    axs[3].plot(path_bfs, c=np.random.rand(3,))
-    axs[3].set_title("Breadth-First Search Paths")
-    axs[4].plot(path_stw, c=np.random.rand(3,))
-    axs[4].set_title("Spanning Tree Walk Paths")
-    axs[5].plot(path_dfs, c=np.random.rand(3,))
-    axs[5].set_title("Depth-First Search Paths")
+    views.addFigure(3, fig_title)
+    views.addPlotToAxs(path_dfs, fig_title, 0, "Depth-First Search")
+    views.addPlotToAxs(path_bfs, fig_title, 1, "Breadth-First Search")
+    views.addPlotToAxs(path_stw, fig_title, 2, "Spanning Tree Walk")
+
+    views.show()
 
 
 def main():
@@ -226,28 +171,8 @@ def main():
 
 if __name__ == "__main__":
 
-    env = World(40, 40, 0.2)
-    dataviz(env, False, 25, "40x40x0.2 - No Diagonals")
+    env = World(70, 20, 0.25)
+    dataviz(env, False, 25, "70x20x0.25 - No Diagonals")
 
-    env2 = World(50, 20, 0.2)
-    dataviz(env2, False, 25, "50x20x0.2 - No Diagonals")
-
-    env = World(40, 40, 0.2)
-    dataviz(env, False, 25, "40x40x0.5 - No Diagonals")
-
-    env2 = World(50, 20, 0.2)
-    dataviz(env2, False, 25, "50x20x0.5 - NoDiagonals")
-
-    env = World(40, 40, 0.2)
-    dataviz(env, True, 25, "40x40x0.2 - Diagonals")
-
-    env2 = World(50, 20, 0.2)
-    dataviz(env2, True, 25, "50x20x0.2 - Diagonals")
-
-    env = World(40, 40, 0.2)
-    dataviz(env, True, 25, "40x40x0.5 - Diagonals")
-
-    env2 = World(50, 20, 0.2)
-    dataviz(env2, True, 25, "50x20x0.5 - Diagonals")
-
-    plt.show()
+    env = World(20, 70, 0.25)
+    dataviz(env, False, 25, "20x70x0.25 - No Diagonals")
