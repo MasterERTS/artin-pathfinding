cdb72b79f7e27f751b73553b35b204f5397c4aee
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-03-27 18:39:03 +0100

    🚧 wip : pathfinding api

---
+++ lib/pathfinder.py
@@ -0,0 +1,23 @@
+from lib import *
+
+class PathFinding():
+    def __init__(self):
+        pass
+
+    def getPathBFS(self, env):
+        pass
+
+    def getPathDFS(self, env):
+        pass
+
+    def getPathSTW(self, env):
+        pass
+
+    def getPathAStar(self, env):
+        pass
+
+    def getPathDijkstra(self, env):
+        pass
+
+    def getPathBidirAStar(self, env):
+        pass
